3e94e0da158be9a13c07cf777a33f6fee5082518
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-13 09:45:09 -0300

    Class Department enteties

--- src/db/Program.java
+++ src/db/Program.java
@@ -1,12 +1,17 @@
 package db;
 
+import java.util.Date;
+
 import db.model.entites.Department;
+import db.model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "Book");
-		System.out.println(department);
+		Seller seller = new Seller(1, "Antonio", "example@email.com", new Date(), department);
+		
+		System.out.println(seller);
 		
 	}
 
