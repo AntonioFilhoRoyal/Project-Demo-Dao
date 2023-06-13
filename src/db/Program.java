da801b10152c7565f0f7513f0b2732960b4e43ea
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-13 09:22:40 -0300

    Class Department enteties

--- src/db/Program.java
+++ src/db/Program.java
@@ -1,36 +1,12 @@
 package db;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.connector.DbConnect;
+import db.model.entites.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Connection connect = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
-		try {
-			connect = DbConnect.getConnection();
-			st = connect.createStatement();
-			rs = st.executeQuery("Select * from department");
-			
-			while(rs.next()) {
-				System.out.println("ID: " + rs.getInt("Id") + 
-									", Name: " + rs.getString("Name"));
-			}
-		} catch(SQLException e) {
-			e.printStackTrace();
-		} finally {
-			DbConnect.closeConnection();
-			DbConnect.closeStatement(st);
-			DbConnect.closeResultSet(rs);
-		}
+		Department department = new Department(1, "Book");
+		System.out.println(department);
 		
 	}
 
